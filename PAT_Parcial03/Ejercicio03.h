a7a163582c028295fa20da7dbc8260f542c9ce19
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-11-25 21:49:46 -0600

    Update Ejercicio03.h

--- PAT_Parcial03/Ejercicio03.h
+++ PAT_Parcial03/Ejercicio03.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 #include <unordered_map>
 #include <map>
@@ -18,29 +17,5 @@ public:
 private:
     unordered_map<string, map<int, string>> data;
 };
-
-TimeMap::TimeMap()
-{
-}
-
-void TimeMap::set(string key, string value, int timestamp)
-{
-    data[key][timestamp] = value;
-}
-
-string TimeMap::get(string key, int timestamp)
-{
-    if (data.find(key) == data.end()) {
-        return "";
-    }
-
-    auto it = data[key].upper_bound(timestamp);
-    if (it == data[key].begin()) {
-        return "";
-    }
-
-    --it;
-    return it->second;
-}
 
 

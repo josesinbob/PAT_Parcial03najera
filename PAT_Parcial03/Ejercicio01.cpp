f22a69cc5ad4d895b85b15fed41cef34e68432e0
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-11-25 21:37:31 -0600

    Update Ejercicio01.cpp

--- PAT_Parcial03/Ejercicio01.cpp
+++ PAT_Parcial03/Ejercicio01.cpp
@@ -1,6 +1,50 @@
 #include "Ejercicio01.h"
+#include <iostream>
+#include <unordered_map>
+#include <map>
+
+Node<int>* mergeSortedLists(Node<int>* l1, Node<int>* l2)
+{
+    Node<int> header{ 0 };
+    Node<int>* current = &header;
+
+    while (l1 != nullptr && l2 != nullptr) 
+    {
+        if (l1->value > l2->value) 
+        {
+            current->next = l1;
+            l1 = l1->next;
+        }
+        else 
+        {
+            current->next = l2;
+            l2 = l2->next;
+        }
+        current = current->next;
+    }
+
+    if (l1 != nullptr) 
+    {
+        current->next = l1;
+    }
+    else
+    {
+        current->next = l2;
+    }
+
+    return header.next;  
+}
 
 Node<int>* Ejercicio01::mergeLists(Node<Node<int>*>* lists)
 {
-    return nullptr;
+    if (lists == nullptr) {
+        return nullptr;
+    }
+
+    while (lists->next != nullptr) {
+        lists->value = mergeSortedLists(lists->value, lists->next->value);
+        lists->next = lists->next->next;
+    }
+
+    return lists->value;
 }

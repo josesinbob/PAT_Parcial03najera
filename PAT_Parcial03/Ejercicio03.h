a33e82d290f6339af88417fbfa095e5c694a4898
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-11-25 21:46:22 -0600

    Update Ejercicio03.h

--- PAT_Parcial03/Ejercicio03.h
+++ PAT_Parcial03/Ejercicio03.h
@@ -1,16 +1,46 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+#include <map>
 
-using std::string;
+using namespace std;
 
 class TimeMap
 {
 public:
-	TimeMap();
+    TimeMap();
 
-	void set(string key, string value, int timestamp);
+    void set(string key, string value, int timestamp);
 
-	string get(string key, int timestamp);
+    string get(string key, int timestamp);
+
+private:
+    unordered_map<string, map<int, string>> data;
 };
+
+TimeMap::TimeMap()
+{
+}
+
+void TimeMap::set(string key, string value, int timestamp)
+{
+    data[key][timestamp] = value;
+}
+
+string TimeMap::get(string key, int timestamp)
+{
+    if (data.find(key) == data.end()) {
+        return "";
+    }
+
+    auto it = data[key].upper_bound(timestamp);
+    if (it == data[key].begin()) {
+        return "";
+    }
+
+    --it;
+    return it->second;
+}
+
 
